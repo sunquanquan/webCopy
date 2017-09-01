0fff3a06e895f532dd618c6d07d54b3b5938b332
孙泉泉 <393980327@qq.com>
2017-09-01 17:42:28 +0800

    clear

--- public/js/32.js
+++
@@ -1,5 +0,0 @@
-var specialArray = [];
-	specialArray[0] = {'url':'http://qy.iwgame.com/comprehensive/news/2016/1116/535.html','alt':'权御天下荣膺金翎奖最期待网游！','imgurl':'http://www-res.iwgame.com/site2/2016/1130/20161130101719775.jpg'};
-	specialArray[1] = {'url':'http://hd.shumenol.com/special/syjl/','alt':'3V3竞技玩法发布 新羽翼唯美上线','imgurl':'http://www-res.iwgame.com/site2/2016/1129/20161129054202485.jpg'};
-	specialArray[2] = {'url':'http://act3.zuixiaoyao.com/xweber/thanks2016/index.htm','alt':'特权新服【权倾风华】 感恩开启！','imgurl':'http://www-res.iwgame.com/site2/2016/1129/20161129050755207.jpg'};
-	specialArray[3] = {'url':'http://hd.tzj.iwgame.com/special/ss201611/index.html','alt':'新服【盛世】14大活动引爆新区新服','imgurl':'http://www-res.iwgame.com/site2/2016/1129/20161129045852351.jpg'};
--- public/js/poster_js/49.js
+++
@@ -1,6 +0,0 @@
-var productPoster = [];
-productPoster[0] = {
-    "url": "",
-    "title": "",
-    "imgurl": "http://www-res.iwgame.com/site2/2017/0516/20170516054520683.png"
-};
