7409d264bc1dedd8d664fcaaa6a0b67533c858e9
孙泉泉 <393980327@qq.com>
2017-09-01 18:17:42 +0800

    tool

---
+++ tools/getPool.js
@@ -0,0 +1,18 @@
+/**
+ * Created by quanquan.sun on 2017/8/31.
+ */
+var mysql = require('mysql');
+var fs = require('fs');
+
+//DB连接池获取
+var gameConfig = JSON.parse(fs.readFileSync('./config/webConfig.json', "utf8"));
+var pool = mysql.createPool(gameConfig.mysqlOptions);
+var getConnection = function (callback){
+    pool.getConnection(function (err, connection) {
+        if(err) console.log("conncetion err",err);
+        // 回调处理逻辑
+        callback(err,connection);
+    });
+};
+
+exports.getConnection = getConnection;
